d2763d691833d8f2dd506e3a5d032d914006ac04
vickram7 <2000080171@kluniversity.in>
2023-12-26 13:16:33 +0530

    Creating Header

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,18 @@ import signin from './pages/signin';
 import signup from './pages/signup';
 import about from './pages/about';
 import profile from './pages/profile';
+import Header from './components/Header';
 export default function App() {
   return(
     <BrowserRouter>
+    <Header />
     <Routes>
-      <Route path='/' element={<home/>} />
-      <Route path="/signin" element={<signin/>} />
-      <Route path="/signup" element={<signup/>} />
-      <Route path="/about" element={<about/>} />
-      <Route path="/profile" element={<profile/>} / >
+      <Route path='/' element={home()} />
+      <Route path="/signin" element={signin()} />
+      <Route path="/signup" element={signup()} />
+      <Route path="/about" element={about()} />
+      <Route path="/profile" element={profile()} / >
     </Routes>
     </BrowserRouter>
-    )
+    );
 }
