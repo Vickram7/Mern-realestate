ff6f62ceb4fb1198bf576ab2bc79f7ee8439f1d6
vickram7 <2000080171@kluniversity.in>
2023-12-27 16:59:38 +0530

    ui for signup

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -3,7 +3,7 @@ import { Link} from 'react-router-dom';
 export default function signup() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
     <form className="flex flex-col gap-4">
       <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username"/>
       <input type="email" placeholder="email" className="border p-3 rounded-lg" id="email"/>
