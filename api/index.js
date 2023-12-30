5208571f6e20e669e1101b4947d6a691872c600e
vickram7 <2000080171@kluniversity.in>
2023-12-30 17:02:43 +0530

    create update user API

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config().then
 mongoose.connect(process.env.MONGO).then(()=>{console.log('Connected to DB!');}).catch((err)=>{console.log(err)});
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
 app.listen(3000,() =>{
     console.log('Server is running on port 3000');
 }
